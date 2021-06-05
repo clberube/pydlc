f28d1b3e6431e8fe523cd935ec15236f333f1541
clberube <charleslberube@gmail.com>
2021-06-05 10:59:03 -0400

    Upload initial files

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # PyDenseLines
 Python implementation of the Density Line Chart by Moritz &amp; Fisher. Useful to visualize a large quantity of time series on a single chart.
+
+
+## Example
+
+```python
+import numpy as np
+import matplotlib.pyplot as plt
+from dlines import dense_lines
+
+# Generate random synthetic time series
+x = np.linspace(0, 100, 50)
+ys = []
+for _ in range(1000):
+    ys.append(3 + 1.5*np.random.randn(1)*np.exp(-x/100))
+
+# Plot here
+fig, axs = plt.subplots(1, 2, figsize=(8, 3), sharey=True, sharex=True)
+axs[0].plot(x, np.array(ys).T, lw=1)
+im = dense_lines(ys, x, cmap='magma', ax=axs[1])
+plt.colorbar(im)
+plt.tight_layout()
+plt.show()
+```
+
+[!img](figures/example.png)
---
+++ denselines/DenseLines.py
@@ -0,0 +1,50 @@
+# @Author: charles.berube@polymtl.ca
+# @Date:   2021-06-05 09:06:98
+# @Last modified by:   charles.berube@polymtl.ca
+# @Last modified time: 2021-06-05 09:06:15
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def dense_lines(ys, x=None, ny=100, ax=None, pad_y=0.01, normalize=True,
+                **kwargs):
+
+    if ax is None:
+        ax = plt.gca()
+
+    if isinstance(ys, list):
+        ys = np.array(ys)
+
+    assert isinstance(ys, np.ndarray), (
+           "`ys` must be a list of 1D arrays or a 2D array")
+
+    assert pad_y > 0, (
+           "`pad_y` must be greater than 0")
+
+    if x is None:
+        x = np.arange(ys.shape[1])
+
+    kwargs.setdefault('aspect', 'auto')
+    kwargs.setdefault('origin', 'lower')
+
+    drange = ys.max() - ys.min()
+    gridy = np.linspace(ys.min() - pad_y*drange,
+                        ys.max() + pad_y*drange,
+                        ny)
+    gridx = np.linspace(x.min(),
+                        x.max(),
+                        len(x))
+    grid = np.zeros((len(gridy), len(gridx)))
+
+    for y in ys:
+        idx = np.searchsorted(gridy, y) - 1
+        grid[idx, range(len(x))] += 1
+
+    if normalize:
+        grid /= grid.max()
+
+    extent = (gridx.min(), gridx.max(), gridy.min(), gridy.max())
+    img = ax.imshow(grid, extent=extent, **kwargs)
+    return img
