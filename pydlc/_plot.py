26b60419483879f141fff882f59c2e5d379743d2
clberube <charleslberube@gmail.com>
2021-06-06 14:00:44 -0400

    Reduce dimensions of indices

--- pydlc/_plot.py
+++ pydlc/_plot.py
@@ -9,21 +9,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def dense_lines(ys, x=None, ny=100, ax=None, pad_y=0.01, normalize=True,
+def dense_lines(ys, x=None, ax=None, ny=100, y_pad=0.01, normalize=True,
                 **kwargs):
     """Returns a Density Line Chart.
 
     Args:
-        ys (:obj:`list` of :obj:`1darrays`): The lines to plot.
+        ys (:obj:`list` of :obj:`1darray`): The lines to plot. Can also be
+            passed as a `2darray`.
         x (:obj:`1darray`, optional): The x values corresponding to
             the data passed with `ys`. If not provided, range(0, len(ys))
             is used.
+        ax (:obj:`matplotlib axes`, optional): The axes to plot on. If not
+            provided a new figure will be created.
         ny (:obj:`int`, optional): The vertical grid size. Higher values
             yield a smoother density estimation. Lower values may yield a
             pixelated result. Default: 100.
-        ax (:obj:`matplotlib axes`, optional): The axes to plot on. If not
-            provided a new figure will be created.
-        pad_y (:obj:`float`, optional): The padding fraction to establish the
+        y_pad (:obj:`float`, optional): The padding fraction to establish the
             grid limits past the data values. Must be greater than 0.
             Default: 0.01 (1%).
         normalize (:obj:`bool`, optional): Normalize the plot so the density
@@ -43,8 +44,8 @@ def dense_lines(ys, x=None, ny=100, ax=None, pad_y=0.01, normalize=True,
     assert isinstance(ys, np.ndarray), (
            "`ys` must be a list of 1D arrays or a 2D array")
 
-    assert pad_y > 0, (
-           "`pad_y` must be greater than 0")
+    assert y_pad > 0, (
+           "`y_pad` must be greater than 0")
 
     if x is None:
         x = np.arange(ys.shape[1])
@@ -52,25 +53,22 @@ def dense_lines(ys, x=None, ny=100, ax=None, pad_y=0.01, normalize=True,
     kwargs.setdefault('aspect', 'auto')
     kwargs.setdefault('origin', 'lower')
 
-    drange = ys.max() - ys.min()
-    gridy = np.linspace(ys.min() - pad_y*drange,
-                        ys.max() + pad_y*drange,
-                        ny)
-    gridx = np.linspace(x.min(),  # unused for now
-                        x.max(),
-                        x.shape[0])
+    nx = x.shape[0]
+    x_range = np.arange(nx)
+    y_pad *= (ys.max() - ys.min())
 
-    grid = np.zeros((ys.shape[0], gridy.shape[0], gridx.shape[0]))
-    indices = np.searchsorted(gridy, ys) - 1
+    y_grid = np.linspace(ys.min() - y_pad, ys.max() + y_pad, ny)
+    x_grid = np.linspace(x.min(), x.max(), nx)
 
-    for i, idx in enumerate(indices):
-        grid[i, idx, range(len(x))] = 1
+    grid = np.zeros((ny, nx))
+    indices = np.searchsorted(y_grid, ys) - 1
 
-    grid = np.sum(grid, axis=0)
+    for idx in indices:
+        grid[idx, x_range] += 1
 
     if normalize:
         grid /= grid.max()
 
-    extent = (gridx.min(), gridx.max(), gridy.min(), gridy.max())
+    extent = (x_grid.min(), x_grid.max(), y_grid.min(), y_grid.max())
     img = ax.imshow(grid, extent=extent, **kwargs)
     return img
