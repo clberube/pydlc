445004f1e7a5fca24b649306d8a800506e6d9a33
clberube <charleslberube@gmail.com>
2021-06-06 10:29:13 -0400

    Release files

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
   author = 'Charles L. Bérubé',                   # Type in your name
   author_email = 'charles.berube@polymtl.ca',      # Type in your E-Mail
   url = 'https://github.com/clberube/pydlc',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/clberube/pydlc/archive/refs/tags/v0.1.tar.gz',    # I explain this later on
   keywords = ['data visualization', 'density', 'time series'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'numpy',
@@ -23,7 +23,7 @@ setup(
       ],
   classifiers=[
     'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Scientists',      # Define that your audience are developers
+    'Intended Audience :: Science/Research',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',   # Again, pick a license
     'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
